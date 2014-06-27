ecab7a10e4334f0f794fd3407a4bbb4e51826034
James Wilson <jrwilson3@gmail.com>
2014-06-27 14:33:35 -0400

    ROOMS-374: Clean up range widget JS.

--- js/rooms_availability_constraints_range.js
+++ js/rooms_availability_constraints_range.js
@@ -1,10 +1,10 @@
 (function ($) {
   /**
-   * Booking unit options widget JS operations.
+   * Availability Constraint Range options widget JS operations.
    */
-  Drupal.behaviors.rooms_operations_widget = {
+  Drupal.behaviors.rooms_availability_constraint_range_widget = {
     attach: function(context) {
-      $('.remove-booking-option').click(function(e) {
+      $('.rooms-availability-range--remove-button').click(function(e) {
         $(this).parent().find('input[type=text]').val('');
         $(this).parents('tr').hide();
         e.preventDefault();
